6e19bdf28d142ede67f9e7887d3426ff02b3e627
phuc dang <phucsystem@gmail.com>
2019-08-24 14:28:54 +0700

    Add script setup ubuntu

--- web-server/setup_ubuntu.sh
+++ web-server/setup_ubuntu.sh
@@ -6,8 +6,6 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-commom unzip
 
 # Install docker
-wget https://yum.dockerproject.org/gpg
-sudo apt-key add gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
